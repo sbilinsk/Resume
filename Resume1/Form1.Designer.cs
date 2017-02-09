b72eb5df15162a379e93b398ba4813f58c835193
sbilinsk <shaneb1393@gmail.com>
2017-02-08 20:21:04 -0600

    Add files via upload

---
+++ Resume1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Resume1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lstSection = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnDisplay.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDisplay.Location = new System.Drawing.Point(343, 88);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(134, 63);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "&Display";
+            this.btnDisplay.UseVisualStyleBackColor = false;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtOutput.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtOutput.Location = new System.Drawing.Point(4, 205);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(666, 596);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // lstSection
+            // 
+            this.lstSection.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstSection.FormattingEnabled = true;
+            this.lstSection.ItemHeight = 21;
+            this.lstSection.Location = new System.Drawing.Point(128, 69);
+            this.lstSection.Name = "lstSection";
+            this.lstSection.Size = new System.Drawing.Size(199, 130);
+            this.lstSection.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Cambria", 12.2F, System.Drawing.FontStyle.Bold);
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExit.Location = new System.Drawing.Point(611, 16);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(59, 48);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(192, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 39);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Shane Bilinski";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnDisplay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(675, 809);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstSection);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDisplay);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Shane Bilinski Interactive Resume";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ListBox lstSection;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Resume1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resume1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lstSection.Items.Add("Contact Info");
+            lstSection.Items.Add("About Me");
+            lstSection.Items.Add("Summary");
+            lstSection.Items.Add("Education");
+            lstSection.Items.Add("Skills/Qualifications");
+            lstSection.Items.Add("Experience");
+            display("Thanks for taking a look at my interactive resume, written in C#. Please choose a section to view from the drop down list above, then click display");
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+
+            if (lstSection.Text == "Contact Info")
+            {
+                display(
+                "Full Name: Shane Bilinski " + "\r\n" +
+                "Address: 550 W Surf St, Chicago, IL 60657" + "\r\n" +
+                "Cell: (312) 982 - 1200" + "\r\n" +
+                "Email: shaneb1393@gmail.com");
+            }
+
+            else if (lstSection.Text == "Summary")
+            {
+                display("Currently a Technical Consultant employed in SAP financial supply chain automation." + "\r\n" +
+                        "Experienced with database administration, network engineering, C, C#, SQL, and Java");
+                return;
+            }
+
+            else if (lstSection.Text == "About Me")
+            {
+                display("I'm a recent graduate currently living in Chicago, employed as a technical consulant. ");
+            }
+
+
+            else if (lstSection.Text == "Education")
+            {
+                display("Purdue University - West Lafayette" + "\r\n" +
+                        "College of Technology" + "\r\n" +
+                        "Major: Computer and Information Technology" + "\r\n" +
+                        "Concentration: Systems Analysis & Design" + "\r\n" +
+                        "2012 - 2016");
+                return;
+
+            }
+
+            else if (lstSection.Text == "Skills/Qualifications")
+            {
+                display("Programming languages: C#, SQL, C, Python, and Java" + "\r\n" +
+                        "Databases: Oracle SQL Developer, PL / SQL, Access" + "\r\n" +
+                        "Other: SAP, Visio, Microsoft Project, TCP / IP, Visual Studio," +"\r\n" +
+                        "OCR Software, UML, Windows Server, VMWare, IBM SPSS");
+                return;
+
+            }
+
+            else if (lstSection.Text == "Experience")
+            {
+                display("Current Employer: Hanse Orga Group" + "\r\n" +
+                        "Location: Chicago, IL" + "\r\n" +
+                        "Date: July 2016 - Present" + "\r\n" +
+                        "Position: Technical Consultant" + "\r\n" +
+                        "Duties:" + "\r\n" +
+                        "Automate accounting processes in SAP FICO" + "\r\n" +
+                        "Act as technical lead on projects with clients across a wide industry spectrum" + "\r\n" +
+                        "Frequent presentations to key business users" + "\r\n" +
+                        "Work with client technical teams to configure and implement software" + "\r\n" +
+                        "Troubleshoot software configuration" + "\r\n" +
+                        "Work with a team on a project from launch to go - live" + "\r\n" + "\r\n" +
+                        "Employer: Purdue University" + "\r\n" +
+                        "Location: West Lafayette, IN" + "\r\n" +
+                        "Date: January 2015 – May 2016" + "\r\n" +
+                        "Position: Senior Lab Assistant" + "\r\n" +
+                        "Duties:" + "\r\n" +
+                        "Supervised lab assistants and trained new employees" + "\r\n" +
+                        "Managed computer lab staffing and employee performance" + "\r\n" +
+                        "Promoted from lab assistant in minimum time allowed by policy" + "\r\n" +
+                        "Provided technical support for campus computer labs" + "\r\n" + "\r\n" +
+                        "Employer: University of Notre Dame" + "\r\n" +
+                        "Location: South Bend, IN" + "\r\n" +
+                        "Date: Summer 2014 – 2015" + "\r\n" +
+                        "Position: Intern - Office of Development, Information Technology" + "\r\n" +
+                        "Duties:" + "\r\n" +
+                        "Gained meaningful business experience as part of an IT team" + "\r\n" +
+                        "Aided in troubleshooting departmental technology problems" + "\r\n" +
+                        "Installed new computer systems for employees" + "\r\n" +
+                        "Visited employees to assess their needs" + "\r\n" + "\r\n" +
+                        "Employer: University of Notre Dame" + "\r\n" +
+                        "Location: South Bend, IN" + "\r\n" +
+                        "Date: Summer 2015" + "\r\n" +
+                        "Position: Computer Service Technician" + "\r\n" +
+                        "Duties: " + "\r\n" +
+                        "Improved CRM by automating performance surveys, reducing time taken by 90 %" + "\r\n" +
+                        "Repaired software and hardware problems for student and faculty computers" + "\r\n" +
+                        "Created and maintained databases for service records and campus computer orders");
+                return;
+            }
+
+        }
+
+        private void display(string textToOutput)
+        {
+            txtOutput.Text = textToOutput;
+            return;
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstSection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+    }
+}
